80dba5dffa31370e72f5ae45e46fc5eefe902d45
Rahul Badhai <rahulbadhai07@gmail.com>
2025-04-26 20:02:18 +0530

    testing

--- backend/server.js
+++ backend/server.js
@@ -18,12 +18,12 @@ const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
-// const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:5173",
-//     methods: ["GET", "POST"],
-//   },
-// });
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
 // Specify allowed origins
 // const allowedOrigins = [
 //   'https://vizintel-oy2p.vercel.app',  // Vercel production origin
